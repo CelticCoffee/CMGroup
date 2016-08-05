0ab1779547e19db9f6fddb9bd1cd2f41eb521886
Wendy <wendygoodwin@mac.com>
2016-08-05 12:40:19 -0700

    sidebar for project page

--- js/projects.js
+++ js/projects.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
     $('.moreText', this).toggle('slow');
     console.log('hi i am clicked')
     $('.teaser', this).toggle();
+    $('#tableOfContents').toggle();
   })
+
+  var openSide = document.querySelector('#openProjectLst');
+  openSide.addEventListener('click', openSideBar, true);
+
+  var closeSide = document.querySelector('.closeButton');
+  closeSide.addEventListener('click', closeNav, true);
+
+  function openSideBar() {
+    document.getElementById('tableOfContents').style.width = "350px";
+  }
+
+  function closeNav() {
+    document.getElementById('tableOfContents').style.width = "0";
+  }
 
 });
